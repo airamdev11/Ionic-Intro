22474c1cefba419908272c498637525133006138
airamdev11 <airamdev11@outlook.com>
2022-06-28 18:49:37 -0500

    Galeria

--- src/app/services/photo.service.ts
+++ src/app/services/photo.service.ts
@@ -7,7 +7,7 @@ import { Storage } from '@capacitor/storage';
   providedIn: 'root'
 })
 export class PhotoService {
-
+  public photos: UserPhoto[] = [];
   constructor() { }
 
   public async addNewToGallery() {
@@ -17,6 +17,16 @@ export class PhotoService {
       source: CameraSource.Camera,
       quality: 100
     });
+
+    this.photos.unshift({
+      filepath: 'soon...',
+      webviewPath: capturedPhoto.webPath
+    });
   }
 
+}
+
+export interface UserPhoto {
+  filepath: string;
+  webviewPath: string;
 }
